0bd78809e6fcfbb9bbc503ceafb6677c5b443422
ezhang94 <elizabeth.zhang94@gmail.com>
2021-04-21 12:53:05 -0700

    Corrected module reference

--- gimbal/distributions.py
+++ gimbal/distributions.py
@@ -5,7 +5,7 @@ import numpy as onp
 
 from tensorflow_probability.substrates import jax as tfp
 
-from util import log_bessel_iv_asymptotic, log_sinh, coth
+from gimbal.util import log_bessel_iv_asymptotic, log_sinh, coth
 
 CONCENTRATION_REGULARIZER = 1e-8
 VARIANCE_REGULARIZER = 1e-8
