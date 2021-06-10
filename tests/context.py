184e35b779bc87ceacaf14b81a2f94e09859c6e3
Libby Zhang <ezhang94@github.com>
2021-06-10 16:21:22 -0700

    Corrected path reference to source code

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/gimbal/')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../gimbal/')))
 
 import distributions
 import util
